adcb68c720a94dfca5cc3405603ba35689b54bf9
Anuj Thakkar <anuj919@gmail.com>
2013-01-31 10:07:28 +0530

    removing concurrency

--- src/state/ConcurrentState.java
+++ src/state/ConcurrentState.java
@@ -42,11 +42,8 @@ import datastructures.MultiQueue;
  */
 
 public class ConcurrentState implements State {
-	
-	static int NUM_PROC = Runtime.getRuntime().availableProcessors();
-	static ExecutorService threadpool = Executors.newFixedThreadPool(NUM_PROC);
-	
-	Map<EventClass, MultiQueue<ComplexEvent> > map;
+		
+	Map<EventClass, List<ComplexEvent> > map;
 	EventClass outputEventClass;
 	List<EventClass> inputEventClasses;
 	long duration;
@@ -91,10 +88,10 @@ public class ConcurrentState implements State {
 		//this.evaluator = JaninoEvalFactory.fromString(complexType, predicate);
 		this.setPredicate(predicate);
 		
-		map=new HashMap<EventClass, MultiQueue<ComplexEvent>>();
+		map=new HashMap<EventClass, List<ComplexEvent>>();
 		for(EventClass ec : classes ) {
-			MultiQueue<ComplexEvent> multiQueue = new MultiQueue<ComplexEvent>(NUM_PROC);
-			map.put(ec, multiQueue);
+			List<ComplexEvent> list = new LinkedList<ComplexEvent>();
+			map.put(ec, list);
 		}
 		
 		for(EventClass ec: classes) {
@@ -112,107 +109,85 @@ public class ConcurrentState implements State {
 		consumeHeartbit(e.getTimeStamp().getEndTime()); 	// Assuming events are submitted in total order
 		//final Collection<ComplexEvent> toNextStateList = new ConcurrentLinkedQueue<ComplexEvent>();
 		//final Collection<ComplexEvent> toBeAddedList = new ConcurrentLinkedQueue<ComplexEvent>();
-		MultiQueue<ComplexEvent> multiQueue = map.get(eClass);
-		if(multiQueue==null)
+		List<ComplexEvent> list = map.get(eClass);
+		if(list==null)
 			return ;
 		
-		final Collection<Callable<Object>> tasks = new ArrayList<Callable<Object>>();
+		List<ComplexEvent> toBeAddedList = new LinkedList<ComplexEvent>();
+		List<ComplexEvent> toNextStateList = new LinkedList<ComplexEvent>();
 		
 		//generate new partial matches
-		for(int i=0;i<multiQueue.getNumInternalQueue();i++) {
-			final List<ComplexEvent> list = multiQueue.getList(i);
-			tasks.add(Executors.callable( new Runnable() {
-				@Override
-				public void run() {
 					
-					Map<EventClass,List<ComplexEvent>> tempMap = new HashMap<EventClass, List<ComplexEvent>>();
-					for(EventClass eClass : map.keySet()) {
-						tempMap.put(eClass, new LinkedList<ComplexEvent>());
-					}
-					
-					for(Iterator<ComplexEvent> itr=list.iterator(); itr.hasNext();) {
-						ComplexEvent partialMatch = itr.next();
+		for(Iterator<ComplexEvent> itr=list.iterator(); itr.hasNext();) {
+			ComplexEvent partialMatch = itr.next();
 						
-						//check if the partial match is expired?
-						boolean expired = lastHearbitTimeStamp > partialMatch.getPermissibleTimeWindowTill();
-						if(expired) { //expired
-							itr.remove();
-							continue;
-						} 
+			//check if the partial match is expired?
+			boolean expired = lastHearbitTimeStamp > partialMatch.getPermissibleTimeWindowTill();
+			if(expired) { //expired
+				itr.remove();
+				continue;
+			} 
 						
-						ComplexEvent extendedPartialMatch = ComplexEvent.copyOf(partialMatch);
-						int numSubEvents = extendedPartialMatch.addEvent(e);
-						boolean constraintSatisfied = false;
-						boolean moreAttribNeeded = false;
-						try {
-							//constraintSatisfied = evaluator.evaluate(extendedPartialMatch);
-							Integer eventClassesAlreadyPresent = extendedPartialMatch.getEventClassesAlreadyPresent();
-							Evaluator evaluator = evaluators.get(eventClassesAlreadyPresent);
-							if(evaluator == null)
-								constraintSatisfied=true;
-							else
-								constraintSatisfied = evaluator.evaluate(extendedPartialMatch);
-						} catch(NullPointerException ex) {
-						//	//ignore: signifies that not enough values are present to evaluate predicate to be true
-							moreAttribNeeded = true;
-						} catch(NoSuchFieldException nsfe) {
-							throw new RuntimeException("Something wrong with predicate, possibly attribute names");
-						}
-						if(numSubEvents == numClasses) {
-							//if(constraintSatisfied && !extendedPartialMatch.isConsumed()) {
-							if(constraintSatisfied) {
-								extendedPartialMatch.setEventClass(outputEventClass);
-								//toNextStateList.add(extendedPartialMatch);
-								GlobalState.getInstance().submitNext(extendedPartialMatch);
-								//extendedPartialMatch.setConsumed(true);
-								//itr.remove();
-							}
-						} else {
-							/* 
-							 * if(!moreAttribNeeded && !constraintSatisfied)
-							 * 	discard the newEvent
-							 * else
-							 * 	add it to other queues
-							 */
-							if(moreAttribNeeded || constraintSatisfied ) {
-								// toBeAddedList.add(extendedPartialMatch);
-								for(EventClass waitingFor : map.keySet() ) {
-									if(!extendedPartialMatch.containsEventOfClass(waitingFor.getName())) // ce already contains this
-										tempMap.get(waitingFor).add(extendedPartialMatch);
-								}
-							}
-								
-						}
-					}
-					for(java.util.Map.Entry<EventClass,List<ComplexEvent>> entry : tempMap.entrySet())
-						map.get(entry.getKey()).addAll(entry.getValue());
+			ComplexEvent extendedPartialMatch = ComplexEvent.copyOf(partialMatch);
+			int numSubEvents = extendedPartialMatch.addEvent(e);
+			boolean constraintSatisfied = false;
+			boolean moreAttribNeeded = false;
+			try {
+				//constraintSatisfied = evaluator.evaluate(extendedPartialMatch);
+				Integer eventClassesAlreadyPresent = extendedPartialMatch.getEventClassesAlreadyPresent();
+				Evaluator evaluator = evaluators.get(eventClassesAlreadyPresent);
+				if(evaluator == null)
+					constraintSatisfied=true;
+				else
+					constraintSatisfied = evaluator.evaluate(extendedPartialMatch);
+			} catch(NullPointerException ex) {
+				//	//ignore: signifies that not enough values are present to evaluate predicate to be true
+				moreAttribNeeded = true;
+			} catch(NoSuchFieldException nsfe) {
+				throw new RuntimeException("Something wrong with predicate, possibly attribute names");
+			}
+			if(numSubEvents == numClasses) {
+				//if(constraintSatisfied && !extendedPartialMatch.isConsumed()) {
+				if(constraintSatisfied) {
+					extendedPartialMatch.setEventClass(outputEventClass);
+					toNextStateList.add(extendedPartialMatch);
+					//extendedPartialMatch.setConsumed(true);
+					//itr.remove();
 				}
-			} ));	
+			} else {
+				/* 
+				 * if(!moreAttribNeeded && !constraintSatisfied)
+				 * 	discard the newEvent
+				 * else
+				 * 	add it to other queues
+				 */
+				if(moreAttribNeeded || constraintSatisfied ) {
+					toBeAddedList.add(extendedPartialMatch);
+				}
+				
+			}
 		}
-		
-		try {
-	        threadpool.invokeAll(tasks);
-	    } catch (InterruptedException ex) {
-	    	ex.printStackTrace();
-	    }
 		
 		// this new event will also start new partial match
 		ComplexEvent newMatch = new ComplexEvent(outputEventClass);
 		newMatch.addEvent(e);
 		double endts=newMatch.getTimeStamp().getStartTime()+duration;
 		newMatch.setPermissibleTimeWindowTill(endts);
-		//toBeAddedList.add(newMatch);
+		toBeAddedList.add(newMatch);
 		
-		long t1=System.nanoTime();
-		for(EventClass waitingFor : map.keySet() ) {
-			if(!newMatch.containsEventOfClass(waitingFor.getName())) // ce already contains this
-				map.get(waitingFor).add(newMatch);
-		}		
+		//long t1=System.nanoTime();
 		
-		//for(Event generatedEvent:toNextStateList)
-		//	GlobalState.getInstance().submitNext(generatedEvent);
-		long t2=System.nanoTime();
-		System.err.println("Propagting time = "+(t2-t1)+" ns");
+		for(ComplexEvent ce : toBeAddedList) {
+			for(EventClass waitingFor : map.keySet() ) {
+				if(!ce.containsEventOfClass(waitingFor.getName())) // ce already contains this
+					map.get(waitingFor).add(ce);
+			}
+		}
+		
+		for(Event generatedEvent:toNextStateList)
+			GlobalState.getInstance().submitNext(generatedEvent);
+		//long t2=System.nanoTime();
+		//System.err.println("Propagting time = "+(t2-t1)+" ns");
 	}
 		
 	private void consumeHeartbit(double time) {
